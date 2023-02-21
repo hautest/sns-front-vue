2c8fe2863209bfdcaf9f58812e9ec2ffa807cc5b
LeeSeungHun <dos07008@naver.com>
2023-02-21 21:25:04 +0900

    Post 컴포넌트 제작

---
+++ src/components/Post.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+interface PostProps {
+  title: string;
+  content: string;
+  author: string;
+  id: number;
+  comment: {
+    author: string;
+    content: string;
+    id: number;
+  }[];
+}
+
+const { author, content, title, comment } = defineProps<PostProps>();
+</script>
+
+<template>
+  <li class="post">
+    <div class="titleAndAuthor">
+      <h3>
+        {{ title }}
+      </h3>
+      <span>{{ author }}</span>
+    </div>
+    <hr />
+    <div class="content">
+      {{ content }}
+    </div>
+    <hr />
+    <div v-for="item in comment" v-bind:key="item.id" class="comment">
+      <span class="commentAuthor"> {{ item.author }}: </span>
+      <span>
+        {{ item.content }}
+      </span>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+.post {
+  display: flex;
+  flex-direction: column;
+  border: solid 1px gray;
+  padding: 8px;
+  height: 450px;
+}
+.titleAndAuthor {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+hr {
+  width: 100%;
+}
+.content {
+  overflow-y: scroll;
+  height: 100%;
+}
+</style>
